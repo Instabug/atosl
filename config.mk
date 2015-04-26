15ffa662475612959dda42d130c0c5d22adb000a
Ahmad Sherif <me@ahmadsherif.com>
2015-04-26 15:04:51 +0300

    Update {C,LD}FLAGS to make the build work on Linux

--- config.mk
+++ config.mk
@@ -2,8 +2,8 @@ VERSION = 1.1
 
 PREFIX = /usr/local
 
-CFLAGS = -Wall -Werror -O2 -DATOSL_VERSION=\"${VERSION}\"
-LDFLAGS = -ldwarf -liberty
+CFLAGS = -Wall -Werror -O2 -DATOSL_VERSION=\"${VERSION}\" -I/usr/include/libdwarf
+LDFLAGS = -l:libdwarf.a -liberty
 
 CC = cc
 
